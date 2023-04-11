f2fd7d1abd7651722fad6f973e6b3da66af7ffa5
francoisaguirre <fp.isaguirre@alumno.edu.com.ar>
2023-04-11 10:45:08 -0300

    termiando

--- palindrome/TestPalindrome.py
+++ palindrome/TestPalindrome.py
@@ -1,25 +1,25 @@
 import unittest
-from palindrome import palindromo
+from palindrome import palindrome
 
 class TestPalindrome(unittest.TestCase):
     def test_palindrome_simple(self):
-        result = palindromo('neuquen')
+        result = palindrome('neuquen')
         self.assertEqual(result, True)
 
     def test_palindrome_complex(self):
-        result = palindromo('anita laba la tina')   
+        result = palindrome('anita laba la tina')   
         self.assertEqual(result, True)  
         
     def test_palindorme_complex2(self):
-        result = palindromo('ana') 
+        result = palindrome('ana') 
         self.assertEqual(result, True)   
 
     def test_palindorme_complex3(self):
-        result = palindromo('amor a roma') 
+        result = palindrome('amor a roma') 
         self.assertEqual(result, True)
         
     def test_palindorme_complex4(self):
-        result = palindromo('radar') 
+        result = palindrome('radar') 
         self.assertEqual(result, True)    
 
 if __name__ == '__main__':
